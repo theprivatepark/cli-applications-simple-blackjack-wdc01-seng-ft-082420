ab072c5271e487b63b53c51c2e83fd276af5f6fa
Anney Park <anneypark@gmail.com>
2020-08-14 18:29:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,13 +60,6 @@ def runner
   welcome
   card_total = initial_round
   hit?(card_total)
-  binding.pry
-  until card_total > 21
-   
-    binding.pry
-    display_card_total(card_total)
-  end
-  end_game(card_total)
 end
     
 runner
